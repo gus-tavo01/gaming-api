c1496ee85f9d9261e6723c7e2ab5c2959a2d438f
= <gustavoa.loera01@gmail.com>
2020-09-14 17:39:23 -0700

    add auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AppService } from './services/app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('/api/v1/auth')
+export class AuthController {
+  @Post('/login')
+  async login(@Body() authCredentials: any) {
+    return authCredentials;
+  }
+
+  @Post('/register')
+  async register() {}
+}
