64ff8101b0a77ca609ad4a97f03d99826f55545b
Gustavo Loera <gustavoa.loera01@gmail.com>
2020-09-15 23:19:02 -0700

    add fake user service/module

---
+++ src/services/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[];
+
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'john',
+        password: 'changeme',
+      },
+      {
+        userId: 2,
+        username: 'chris',
+        password: 'secret',
+      },
+      {
+        userId: 3,
+        username: 'maria',
+        password: 'guess',
+      },
+      {
+        userId: 4,
+        username: 'yayis',
+        password: 'Password!',
+      },
+    ];
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
