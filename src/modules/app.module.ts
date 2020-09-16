e9a74bf23963c659972527b81f4b04f2ccb7485f
Gustavo Loera <gustavoa.loera01@gmail.com>
2020-09-15 23:20:44 -0700

    add auth on app.module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from '../app.controller';
 import { AuthController } from '../controllers/auth.controller';
 import { AppService } from '../services/app.service';
+import { AuthModule } from './auth.module';
+import { UsersModule } from './users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.production', '.env.local'],
-      isGlobal: true,
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   entities: [],
+    //   synchronize: true,
+    // }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService],
