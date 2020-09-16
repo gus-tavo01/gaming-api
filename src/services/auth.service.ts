9c70319ab0370bbc3d532d8a994c95aa9df97427
Gustavo Loera <gustavoa.loera01@gmail.com>
2020-09-15 23:19:56 -0700

    add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from './users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+
+  public async validateUser(username: string, password: string) {
+    const user = await this.userService.findOne(username);
+
+    if (user && user.password === password) {
+      return user;
+    }
+    return null;
+  }
+
+  public async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    const serviceResponse = {
+      access_token: this.jwtService.sign(payload),
+    };
+    return serviceResponse;
+  }
+}
