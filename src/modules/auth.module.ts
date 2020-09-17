3a86948238afb05c2e746a1912a783b7126b119f
Gustavo Loera <gustavoa.loera01@gmail.com>
2020-09-16 20:59:52 -0700

    use factory instead normal register JwtModule

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -5,15 +5,19 @@ import { AuthService } from 'src/services/auth.service';
 import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 import { LocalStrategy } from 'src/auth/strategies/local.strategy';
 import { UsersModule } from './users.module';
-import { authConstants } from 'src/auth/constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: authConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
