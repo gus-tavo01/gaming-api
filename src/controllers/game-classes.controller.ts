3fc49ba5041f9b2415dd9e0693eec5d3350b5556
Gustavo Loera <gustavoa.loera01@gmail.com>
2020-10-02 00:32:09 -0700

    add swagger controller tag

--- src/controllers/game-classes.controller.ts
+++ src/controllers/game-classes.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { GameClassesService } from 'src/services/game-classes.service';
 
+@ApiTags('game-classes')
 @Controller('/v1/game-classes')
 export class GameClassesController {
   constructor(private readonly gameClassesService: GameClassesService) {}
