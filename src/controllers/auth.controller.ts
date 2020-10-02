f0cb9ffd47242940083b3e8e679458bf16cb6a56
Gustavo Loera <gustavoa.loera01@gmail.com>
2020-10-02 00:31:50 -0700

    add controller tag, swaggerbody schema

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,20 +1,39 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/guards/jwt.auth.guard';
 import { LocalAuthGuard } from 'src/guards/local.auth.guard';
 import { AuthService } from 'src/services/auth.service';
 
+// GALV TODO:
+// move this temporal DTO on a dif folder
+class LoginDto {
+  @ApiProperty()
+  username: string;
+  @ApiProperty()
+  password: string;
+}
+
+@ApiTags('Auth')
 @Controller('/v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login(@Request() req) {
+  async login(@Request() req, @Body() body: LoginDto) {
     const token = await this.authService.login(req.user);
     return token;
   }
 
   // TODO delete this dummy route
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get('/profile')
   async protectedRoute(@Request() req) {
